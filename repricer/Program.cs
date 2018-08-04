0a6b9ad5ba17d6715a76822813b94a58690993e5
Kevin Weidner <weidnerk@gmail.com>
2018-08-04 10:21:37 -0400

    Update logging.

--- repricer/Program.cs
+++ repricer/Program.cs
@@ -1,3 +1,4 @@
+using dsmodels;
 using dsutil;
 using System;
 using System.Collections.Generic;
@@ -10,32 +11,49 @@ namespace repricer
     class Program
     {
         static DataModelsDB db = new DataModelsDB();
+        private static string log_file = "repricer.txt";
+
         static void Main(string[] args)
         {
+            string msg = null;
             Task.Run(async () =>
             {
-                await repricer();
+                msg = await repricer();
             }).Wait();
+            Console.WriteLine(msg);
             Console.ReadKey();
         }
 
-        static async Task repricer()
+        static async Task<string> repricer()
         {
-            foreach(PostedListing listing in db.Listings )
+            int priceDiffer = 0;
+            string subject = "Repricer";
+            string body = null;
+            int i = 0;
+            int count = db.PostedListings.Count();
+            foreach(PostedListing listing in db.PostedListings.ToList())
             {
+                Console.WriteLine((++i) + "/" + count.ToString());
                 var r = await sclib.Scrape.GetDetail(listing.SourceUrl);
-                Console.WriteLine(listing.Title);
-                Console.WriteLine(r.price);
+                //Console.WriteLine(listing.Title);
+                //Console.WriteLine(r.price);
                 if (r.price != listing.SupplierPrice)
                 {
-                    await DSUtil.SendMailDev("kevinw@midfinance.com");
-                    Console.WriteLine("prices different");
+                    ++priceDiffer;
+                    dsutil.DSUtil.WriteFile(log_file, "Prices differ");
+                    dsutil.DSUtil.WriteFile(log_file, string.Format("{0} {1}", listing.ListedItemID, listing.Title));
+                    dsutil.DSUtil.WriteFile(log_file, string.Format("CategoryID: {0}", listing.CategoryID));
+                    dsutil.DSUtil.WriteFile(log_file, string.Format("last price: {0}", r.price));
+                    dsutil.DSUtil.WriteFile(log_file, string.Format("recorded supplier price: {0}", listing.SupplierPrice));
+                    dsutil.DSUtil.WriteFile(log_file, string.Format("{0}", listing.SourceUrl));
+                    dsutil.DSUtil.WriteFile(log_file, string.Empty);
                 }
-                else
-                    Console.WriteLine("prices match");
-
-                Console.WriteLine("");
             }
+            //if (!string.IsNullOrEmpty(body))
+            //    await DSUtil.SendMailDev("kevinw@midfinance.com", subject, body);
+
+            string msg = string.Format("found {0} different prices.", priceDiffer);
+            return msg;
         }
     }
 }
